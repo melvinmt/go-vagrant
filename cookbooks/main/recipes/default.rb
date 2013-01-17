6331fb100c3bdf034299ee22b9ac52351b0c36de
Melvin Tercan <mmtercan@gmail.com>
2013-01-17 08:54:12 -0800

    Update cookbooks/main/recipes/default.rb

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,9 +1,12 @@
+# Installs apt-get
 require_recipe "apt"
 
+# Sync time to prevent syncing issues between VM and local env
 execute "sync-time" do
   command "ntpdate pool.ntp.org"
 end
 
+# update apt-get list
 execute "initial-sudo-apt-get-update" do
   command "apt-get update"
 end
@@ -17,6 +20,7 @@ cookbook_file "/home/vagrant/.bashrc" do
   action :create
 end
 
+# Installs latest stable mongodb
 apt_repository "mongo-10gen" do
   uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
   distribution "dist"
@@ -27,29 +31,34 @@ apt_repository "mongo-10gen" do
 end
 package "mongodb-10gen"
 
+# Add ppa apt repository for go
 apt_repository "gophers-go-ppa" do
   uri          "http://ppa.launchpad.net/gophers/go/ubuntu"
   distribution node['lsb']['codename']
   components   ["main"]
   key          "9AD198E9"
   keyserver    "keyserver.ubuntu.com"
-
   action :add
 end
 
+# This doesn't work yet.. but I keep trying
 execute "export_go_paths" do
   command "export GPATH=/var/www/app"
   command "export PATH=$PATH:/var/www/app/bin"
 end
 
+# Update the apt-get list again
 execute "second-sudo-apt-get-update" do
   command "apt-get update"
 end
 
+# Install go stable
 package "golang-stable" do
   action :install
 end
 
+# Install git
 include_recipe "git"
 
+# Install bazaar
 include_recipe "bazaar"
