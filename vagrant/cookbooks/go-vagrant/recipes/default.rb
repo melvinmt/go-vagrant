ed0c7c44ca6c15705d3c152b467c8f8f72c7d7ed
Melvin Tercan <mmtercan@gmail.com>
2013-07-16 18:36:19 -0700

    Clean up comments

--- vagrant/cookbooks/go-vagrant/recipes/default.rb
+++ vagrant/cookbooks/go-vagrant/recipes/default.rb
@@ -1,11 +1,6 @@
 # Installs apt-get
 require_recipe "apt"
 
-# Sync time to prevent syncing issues between VM and local env
-# execute "sync-time" do
-#   command "ntpdate pool.ntp.org"
-# end
-
 execute "apt-key" do
   command "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
 end
@@ -14,7 +9,6 @@ execute "10gen.list" do
   command "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list"
 end
 
-# update apt-get list
 execute "initial-sudo-apt-get-update" do
   command "apt-get update"
 end
@@ -31,27 +25,6 @@ include_recipe "mongodb::default"
 
 include_recipe "golang::default"
 
-# # Add ppa apt repository for go
-# apt_repository "gophers-go-ppa" do
-#   uri          "http://ppa.launchpad.net/gophers/go/ubuntu"
-#   distribution node['lsb']['codename']
-#   components   ["main"]
-#   key          "9AD198E9"
-#   keyserver    "keyserver.ubuntu.com"
-#   action :add
-# end
-
-# execute "apt-get update" do
-#   action :nothing
-# end
-
-# # Install go stable
-# package "golang-stable" do
-#   action :install
-# end
-
-# Install git
 include_recipe "git"
 
-# Install bazaar
 include_recipe "bazaar"
